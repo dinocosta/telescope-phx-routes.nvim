73926aa216ae758bd9d69f29479d4d39698c4914
dino <dinojoaocosta@gmail.com>
2023-08-24 17:57:46 +0100

    Fix finder for routes without path

--- lua/phx-routes/init.lua
+++ lua/phx-routes/init.lua
@@ -21,12 +21,16 @@ M.phoenix_routes = function(opts)
 				-- 
 				-- For example:
 				--
-				--     api_v1_path   GET   /api/v1/users   AppWeb.API.V1.UserCotntroller   :index
+				--     api_v1_path   GET   /api/v1/users   AppWeb.API.V1.UserController   :index
 				--
 				-- We can match multiple whitespace characters with the `%s+` character class, while using `%S+` yields the 
 				-- opposite result, i.e., matches on all characters except the space character, which should help 
 				-- understand the string used in the `match` function below.
 				local path, method, url, controller, action = line:match('(%S+)%s+(%S+)%s+(%S+)%s+(%S+)%s+(%S+)')
+
+				if path == nil then
+					method, url, controller, action = line:match('(%S+)%s+(%S+)%s+(%S+)%s+(%S+)')
+				end
 
 				return {
 					value = { path = path, method = method, url = url, controller = controller, action = action},
@@ -44,6 +48,7 @@ M.phoenix_routes = function(opts)
 				-- As such, if you need to access, for example, the `url` value you can use `selection["value"]["url"]`.
 				-- You can always use `print(vim.inspect(selection))` to inspect the `selection` variable and what it contains.
 				local selection = action_state.get_selected_entry()
+				print(vim.inspect(selection))
 
 				-- Start a new process to run the `mix phx.routes --info [URL]` command and get its output.
 				-- Match on the output of the command so we can get the file path.
